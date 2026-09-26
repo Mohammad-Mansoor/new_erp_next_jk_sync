8b88d075a9ae11260d85408aa1ef9969822883b3
Anonymous <mmsafi5207@gmail.com>
2026-09-26 20:33:34 +0430

    initital commit

---
+++ jk_sync/jahan_kodak_sync/doctype/cloud_branch_master/cloud_branch_master.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2026, Jahan Kodak and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+class CloudBranchMaster(Document):
+	pass
